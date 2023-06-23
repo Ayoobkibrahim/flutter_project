cef83552ea24220534d3e9813554d8d731fef3b3
Ayoobkibrahim <ayoobkibrahim01@gmail.com>
2023-06-23 12:14:20 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,30 @@ void main(){                              // to run a program
 
 }
 
+
 class FirstPage extends StatelessWidget {  // stateless mean firstpage do not undrgo any state change
   @override
   Widget build(BuildContext context) {     // to create a widget tree (build context-> to monitor the widgets in the widget tree)
     return  Scaffold(
       appBar: AppBar(
+        // centerTitle: true, //set the appBar title to the center of appBar
         title: Text("HELLO"),
           //  style:TextStyle(fontSize:20,color:Colors.blueAccent)),
       ),
-       body:Center (
-         child:Icon(
-           Icons.face_2_outlined,
-           size: 150,
-           color: Colors.blue,
+       body:const Center (
+
+
+         /// multichild widget
+
+
+         child:Column( // for column children will place from top left to bottom left
+           mainAxisAlignment: MainAxisAlignment.center, // to move the children to center of column
+           children: [
+
+             Icon(Icons.sports_soccer_rounded, color: Colors.blueAccent, size: 100,),
+           Text("My Application", style: TextStyle(fontSize: 25),)
+           ],
+
          ),
       ),
     );
